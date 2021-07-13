92fdc3c24a23320baf94ddcd246599a723ecaa99
kor0sensei <averyolmstead@comcast.net>
2021-07-13 14:44:37 -0500

    added add and release animal functionality, refactored animalCar, List, and Provide also added animalDetail and animalForm

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,12 +1,33 @@
-import React from "react"
-import { NavBar } from "./nav/Navbar.js"
-import { ApplicationViews } from "./ApplicationViews.js"
-import "./Kennel.css"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/Navbar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Kennel.css";
 
-//Look carefully at the <article> tag. In React, we add classes to a component with `className` instead of `class`.
 export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
-)
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,28 +1,33 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from 'react-router-dom';
 import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
 
 export const AnimalList = () => {
-  // This state changes when `getAnimals()` is invoked below
-  const { animals, getAnimals } = useContext(AnimalContext)
 
-  //useEffect - reach out to the world for something
-  useEffect(() => {
-    console.log("AnimalList: useEffect - getAnimals")
-    getAnimals()
+    const { getAnimals, animals } = useContext(AnimalContext)
+    const history = useHistory()
 
-  }, [])
+    // Initialization effect hook -> Go get animal data
+    useEffect(()=>{
+        getAnimals()
+    }, [])
 
+    return (
+      <>
+        <h1>Animals</h1>
 
-  return (
-    <div className="animals">
-      {console.log("AnimalList: Render", animals)}
-      {
-        animals.map(animal => {
-          return <AnimalCard key={animal.id} animal={animal} />
-        })
-      }
-    </div>
-  )
+        <button onClick={() => history.push("/animals/create")}>
+          Add Animal
+        </button>
+        <div className="animals">
+          {
+            animals.map(animal => {
+              return <AnimalCard key={animal.id} animal={animal} />
+            })
+          }
+        </div>
+      </>
+    )
 }
